99a132cfbd19fe825742472a92e27eb404f62a7a
Griffin Corriher <57311125+griffincorriher@users.noreply.github.com>
2023-06-30 21:11:46 -0400

    added navbar and initial model for users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import Head from "next/head";
 import { api } from "~/utils/api";
-import { SignInButton, SignOutButton, SignUpButton, useUser } from "@clerk/nextjs"
-
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
-  const user = useUser();
   
   return (
     <>
@@ -15,13 +12,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {user.isSignedIn ? <SignOutButton>
-          <span className="text-white">Sign Out</span>
-          </SignOutButton> : 
-        <SignInButton>
-          <span className="text-white" >Sign In</span>
-        </SignInButton> 
-        }   
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             IndonesiansNC.com
